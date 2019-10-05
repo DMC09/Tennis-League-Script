b42f1b73db54c1107c021241bf44417548278fe7
Dharmin Chauhan <43209397+DMC09@users.noreply.github.com>
2019-10-04 20:38:07 -0500

    Script upload

---
+++ tennis.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+import time
+
+
+#  set up
+#  Script
+
+
+user = "dharminlive12@live.com"
+pwd = "dhacha2018"
+browser = webdriver.Chrome("/Users/dharminchauhan/Downloads/chromedriver")
+browser.get('http://mon.pythonanywhere.com/accounts/login/?next=/league/')
+elem = browser.find_element_by_id("id_username")
+elem.send_keys(user)
+elem = browser.find_element_by_id("id_password")
+elem.send_keys(pwd)
+elem.send_keys(Keys.RETURN)
+browser.implicitly_wait(10)
+time.sleep(0.5)
+elem = browser.find_element_by_xpath("/html/body/div[3]/h4/form/input[3]")
+elem.click()
+browser.implicitly_wait(5)
+browser.quit()
